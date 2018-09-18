dda2eaa9c11a72516573ae8ec14dcd8aa59ed76a
Chris Zacharias <christopher.zacharias@gmail.com>
2018-09-18 10:27:17 -0700

    First commit.

---
+++ compile.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python -u
+
+import json
+import os.path
+import tornado
+import tornado.template
+
+FILE_ROOT = os.path.normpath(os.path.dirname(__file__))
+TEMPLATE_FILE = "index.html.tmpl"
+DATA_FILE = "data.json"
+
+template_loader = tornado.template.Loader(FILE_ROOT)
+template = template_loader.load(TEMPLATE_FILE)
+
+with open(os.path.join(FILE_ROOT, DATA_FILE), "r") as fh:
+	template_vars = json.load(fh)
+
+print template.generate(**template_vars)
---
+++ main.js
@@ -0,0 +1,15 @@
+function changeSplashPage(button) {
+	var href = button.href;
+	var pos = href.indexOf("#");
+	if (pos < 0 || pos == (href.length + 1)) return false;
+	var target = document.getElementById(href.substring(pos + 1));
+	if (target === null) return false;
+	var current = document.querySelectorAll("#splash-content article.show");
+	for (var i = 0; i < current.length; i++) current[i].className = null;
+	target.className = "show";
+	return false;
+}
+
+window.onLoad = function() {
+
+}
