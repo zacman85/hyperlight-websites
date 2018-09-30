9d97745b6841b3683eea30e7c6772902dc5713fd
Chris Zacharias <christopher.zacharias@gmail.com>
2018-09-29 21:59:52 -0400

    Organizing stuff.

--- compile.py
+++ compile.py
@@ -6,7 +6,7 @@ import tornado
 import tornado.template
 
 FILE_ROOT = os.path.normpath(os.path.dirname(__file__))
-TEMPLATE_FILE = "index.html.tmpl"
+TEMPLATE_FILE = "./templates/index.html.tmpl"
 DATA_FILE = "data.json"
 
 template_loader = tornado.template.Loader(FILE_ROOT)
