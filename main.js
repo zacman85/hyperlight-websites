a56f2a9eb7d29e85a5e9bd77588e304bf273b30a
Chris Zacharias <christopher.zacharias@gmail.com>
2018-10-02 09:10:49 -0400

    More updates.

--- main.js
+++ main.js
@@ -11,13 +11,26 @@ function changeSplashPage(button) {
 }
 
 function changeBodyStyleModel(select) {
-	console.log(select.value);
+	var make = select.value;
+	var template = document.querySelector("#models-template-" + make);
+	var clone = document.importNode(template.content, true);
+	var drop = document.querySelector("#body-style-model");
+		drop.innerHTML = "";
+		drop.appendChild(clone);
 	return false;
 }
 
 function handleSearchForm(form) {
 	var searchQuery = document.getElementById("search-box").value;
 	document.location.href = "https://www.caranddriver.com/search/" + window.encodeURIComponent(searchQuery);
+	return false;
+}
+
+function handleResearchForm(form) {
+	console.log(form);
+	var make = document.getElementById("body-style-make").value;
+	var model = document.querySelector("#body-style-model>select").value
+	console.log(make + " " + model);
 	return false;
 }
 
